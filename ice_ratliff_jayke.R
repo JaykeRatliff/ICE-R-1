aa652374faa1b130ccaf7d72fb99f9dfab232374
JaykeRatliff <112008581+JaykeRatliff@users.noreply.github.com>
2022-09-07 18:02:54 -0500

    Add files via upload

---
+++ ice_ratliff_jayke.R
@@ -0,0 +1,62 @@
+x="hometown"=("Red Rock")
+i=("641945")
+t = 23
+u = 10
+j = t+u
+print("j")
+a=b=q=r = "conflict"
+
+x = c("grape", "mango", "lemon")
+y = 21.4
+f = TRUE
+g = 'covid-19'
+
+typeof(x)
+typeof(y)
+typeof(f)
+typeof(g)
+
+x = 0
+j = 7
+
+comparison = function(arg1, arg2) {
+  t = 3
+  u = 5
+  v = 0
+  if(arg1 > arg2) {
+    v = t * u
+    result_text = paste('arg1 is greater than arg2 and ', v)
+  } else {
+    v = t + u
+    result_text = paste('arg1 is not greater than arg2 and ', v)
+  }
+  return(result_text)
+}
+
+  if(arg1 < arg2) {
+    v = t + u
+    result_text = paste'('arg2 is greater than arg1 and ',v)
+  } else{
+    v = t + u
+		result_text = paste('arg1 is not greater than arg2 and ', v)
+	}
+	return(result_text)"v is less than 10"
+}
+
+z = comparison(x, j)
+
+print(z)
+
+lunches = c('chicken','soy sauce','sugar','sesame oil')
+for(lunches in lunches){
+ if(lunches=="sugar"){
+   break
+ }
+  print(lunches)
+}
+
+
+
+lunches = c('chicken','soy sauce','sugar','sesame oil')
+for(lunches in lunches){
+  print(paste("Food",lunches))}
